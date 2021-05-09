a50219badb5e8161709dfcc65d7045335bd99330
Camila Carraro <51088443+cacarraro@users.noreply.github.com>
2021-05-09 12:39:58 -0300

    Add files via upload

---
+++ InverteString.java
@@ -0,0 +1,35 @@
+package invercao;
+
+public class InverteString
+{
+    public InverteString() {
+    }
+
+    public char[] inversao(String s)
+    {
+        char[] caracteres = s.toCharArray();
+        for (int i = 0; i < caracteres.length/2; i++)
+        {
+            char aux = caracteres[i];
+            if (i == 0)
+            {
+                caracteres[i] = caracteres[caracteres.length-1];
+                caracteres[caracteres.length-1] = aux;
+            }
+            else
+                {
+                    caracteres[i] = caracteres[(caracteres.length - 1) - i];
+                    caracteres[(caracteres.length - 1) - i] = aux;
+                }
+        }
+        return caracteres;
+    }
+}
+class Main {
+    public static void main(String[] args)
+    {
+        InverteString i = new InverteString();
+
+        System.out.println(i.inversao("string para teste"));
+    }
+}
